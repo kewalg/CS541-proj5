0bb5e88586701d77e031cc73626434b1a7bbe2ca
kewalg <kewalgulve007@gmail.com>
2019-11-04 14:26:24 -0500

    sending content of notes to new activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    ArrayAdapter arrayAdapter;
+    static ArrayList<String> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv = findViewById(R.id.lv_notes);
+        notes.add("Example");
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+        lv.setAdapter(arrayAdapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this, NoteActivity.class);
+                intent.putExtra("noteid", i);
+                startActivity(intent);
+            }
+        });
     }
 }
